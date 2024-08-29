0e3d94aa0df2b384dfcb9163905f419fc6dc4034
ItzTas <talessabini2@gmail.com>
2024-08-29 13:15:12 -0400

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"os"
@@ -18,24 +19,41 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	defer l.Close()
+	defer closeIt(l, "")
 
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	defer conn.Close()
 
-	buf := make([]byte, 2048)
+	for {
 
-	_, err = conn.Read(buf)
-	if err != nil {
-		log.Fatal(err)
+		buf := make([]byte, 2048)
+
+		_, err = conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				conn, err = l.Accept()
+				if err != nil {
+					fmt.Println("Error accepting connection: ", err.Error())
+					os.Exit(1)
+				}
+				continue
+			}
+			log.Fatalf("Error reading from conection: %v", err)
+		}
+
+		_, err = conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			log.Fatalf("Error writing to conection: %v", err)
+		}
 	}
+}
 
-	_, err = conn.Write([]byte("+PONG\r\n"))
+func closeIt(c io.Closer, msg string) {
+	err := c.Close()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err, " ", msg)
 	}
 }
